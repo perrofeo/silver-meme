b6c939929f8d7dfd679aba6078efd7d6a8cae682
igor <perrofeofilms@gmail.com>
2019-04-02 15:21:53 -0600

    -

--- pages/portafolio.vue
+++ pages/portafolio.vue
@@ -166,6 +166,14 @@ modelos y trabajos realizados." >
                 Modelos Glammy
             </p>
         </b-card>
+        <b-card :img-src="require('../static/7.jpg')"
+                img-fluid
+                img-alt="image"
+                img-top>
+            <p class="card-text">
+                Modelos Glammy
+            </p>
+        </b-card>
     </b-card-group>
         </b-container>
     </section>
