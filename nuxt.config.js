8212bb8f64443b1a3cc65bfc7eb3a89503473f9e
igor <perrofeofilms@gmail.com>
2019-04-04 15:09:59 -0600

    -

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins:300,400,700' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins:300,400,600,700' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,800' },
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.0/css/all.css', integrity:'sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h', crossorigin:'anonymous' }
     ]
